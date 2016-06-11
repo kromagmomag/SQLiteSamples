bc43b9cd979324c0f80f3e2cdce6a56f4e7db2bf
Kromag Momag <Kromag Momag@KROMAGMOMAG-PC>
2016-06-11 10:19:13 -0400

    Updating recent changes... --- W.J.L. III

--- SQLiteSamples1/SQLiteSamples1/Program.cs
+++ SQLiteSamples1/SQLiteSamples1/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.SQLite.Linq;
+
+
 
 namespace SQLiteSamples1
 {
@@ -10,7 +14,35 @@ namespace SQLiteSamples1
     {
         static void Main(string[] args)
         {
-
+            CheckARGS(args);
         }
+
+        private static void CheckARGS(string[] args)
+        {
+            foreach (var item in args)
+            {
+                if (item.Contains("+db/"))
+                {
+                    CreateDB();
+                }
+            }
+        }
+
+        private static void CreateDB()
+        {
+            if (!System.IO.File.Exists("MyDatabase.sqlite"))
+            {
+                SQLiteConnection.CreateFile("MyDatabase.sqlite");
+                Console.WriteLine($"file was create {DateTime.Now}{Environment.NewLine}Please Press any key!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("file already exists!");
+                Console.ReadKey();
+            }
+        }
+
+
     }
 }
